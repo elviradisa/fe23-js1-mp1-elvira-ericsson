351126f4395d4a6ef4bc0c4b52f8ac9d1bbc0ce9
Elvira Ericsson <elvira.rigi@hotmail.com>
2023-11-13 08:52:55 +0100

    js file

---
+++ js/index.js
@@ -0,0 +1,28 @@
+for (let i = 0; i < 5; i++) {
+    const h1 = document.createElement('h1');
+    document.body.appendChild(h1);
+    h1.innerText = i+1;
+    const hue = 100 + i*20;
+    h1.style.backgroundColor = `hsl(${hue}, 50%, 80%)`
+    h1.style.color = 'purple';
+    h1.style.textAlign = 'center';
+    h1.style.fontSize = `calc(16px + ${i * 10}%)`;
+}
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+container.style.border = '1px solid';
+container.style.height = '200px';
+container.style.display = 'flex';
+container.style.justifyContent = 'space-around';
+container.style.alignItems = 'center';
+
+for (let i = 1; i <= 3; i++) {
+    const innerDiv = document.createElement('div');
+    innerDiv.classList.add('innerDiv');
+    innerDiv.innerText = `Child ${i}`
+
+    container.appendChild(innerDiv);
+
+    innerDiv.style.backgroundColor = 'purple';
+}
